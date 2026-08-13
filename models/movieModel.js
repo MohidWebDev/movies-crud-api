74da9d6515f4e62ef55f684555b0dc7f28c59fdf
Mohid Yaseen <mmohid069@gmail.com>
2026-08-13 12:34:39 +0500

    feat: add movie model with in-memory CRUD operations

---
+++ models/movieModel.js
@@ -0,0 +1,54 @@
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const dataPath = join(__dirname, "../data/movies.json");
+
+let movies = JSON.parse(readFileSync(dataPath, "utf-8"));
+
+const getAllMovies = () => movies;
+
+const getMovieById = (id) => movies.find((movie) => movie.id === Number(id));
+
+const createMovie = (movieData) => {
+  const newMovie = {
+    id: movies.length ? movies[movies.length - 1].id + 1 : 1,
+    ...movieData,
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
+
+const updateMovie = (id, movieData) => {
+  const index = movies.findIndex((movie) => movie.id === Number(id));
+  if (index === -1) return null;
+
+  movies[index] = { id: Number(id), ...movieData };
+  return movies[index];
+};
+
+const patchMovie = (id, updates) => {
+  const index = movies.findIndex((movie) => movie.id === Number(id));
+  if (index === -1) return null;
+
+  movies[index] = { ...movies[index], ...updates };
+  return movies[index];
+};
+
+const deleteMovie = (id) => {
+  const index = movies.findIndex((movie) => movie.id === Number(id));
+  if (index === -1) return false;
+
+  movies.splice(index, 1);
+  return true;
+};
+
+export default {
+  getAllMovies,
+  getMovieById,
+  createMovie,
+  updateMovie,
+  patchMovie,
+  deleteMovie,
+};
