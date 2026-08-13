400a5958cf1d81af32e9b82075351041365f7abb
Mohid Yaseen <mmohid069@gmail.com>
2026-08-13 12:58:16 +0500

    feat: add Mongoose movie schema and model

--- models/movieModel.js
+++ models/movieModel.js
@@ -1,62 +1,24 @@
-import { readFileSync, writeFileSync } from "fs";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import mongoose from "mongoose";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const dataPath = join(__dirname, "../data/movies.json");
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+});
 
-let movies = JSON.parse(readFileSync(dataPath, "utf-8"));
+const Movie = mongoose.model("Movie", movieSchema);
 
-const saveMovies = () => {
-  writeFileSync(dataPath, JSON.stringify(movies, null, 2));
-};
-
-const getAllMovies = () => movies;
-
-const getMovieById = (id) => movies.find((movie) => movie.id === Number(id));
-
-const createMovie = (movieData) => {
-  const newMovie = {
-    id: movies.length ? movies[movies.length - 1].id + 1 : 1,
-    ...movieData,
-  };
-  movies.push(newMovie);
-  saveMovies();
-  return newMovie;
-};
-
-const updateMovie = (id, movieData) => {
-  const index = movies.findIndex((movie) => movie.id === Number(id));
-  if (index === -1) return null;
-
-  movies[index] = { id: Number(id), ...movieData };
-  saveMovies();
-  return movies[index];
-};
-
-const patchMovie = (id, updates) => {
-  const index = movies.findIndex((movie) => movie.id === Number(id));
-  if (index === -1) return null;
-
-  movies[index] = { ...movies[index], ...updates };
-  saveMovies();
-  return movies[index];
-};
-
-const deleteMovie = (id) => {
-  const index = movies.findIndex((movie) => movie.id === Number(id));
-  if (index === -1) return false;
-
-  movies.splice(index, 1);
-  saveMovies();
-  return true;
-};
-
-export default {
-  getAllMovies,
-  getMovieById,
-  createMovie,
-  updateMovie,
-  patchMovie,
-  deleteMovie,
-};
+export default Movie;
