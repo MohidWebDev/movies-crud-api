98d9c628a89a0403a435d4c0904397c7580ad047
Mohid Yaseen <mmohid069@gmail.com>
2026-08-27 10:47:55 +0500

    changed the page limit from 10 to 12

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -5,7 +5,7 @@ import cloudinary from "../config/cloudinary.js";
 
 const getAllMovies = catchAsync(async (req, res) => {
   const page = Math.max(Number(req.query.page) || 1, 1);
-  const limit = Math.min(Math.max(Number(req.query.limit) || 10, 1), 100);
+  const limit = Math.min(Math.max(Number(req.query.limit) || 12, 1), 100);
   const skip = (page - 1) * limit;
 
   const filter = {};
