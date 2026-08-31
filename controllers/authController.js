215d4291bca9154e16fff3e02cd7ca8db741e62a
Mohid Yaseen <mmohid069@gmail.com>
2026-08-31 13:17:37 +0500

    feat(auth): add register controller with duplicate email check

---
+++ controllers/authController.js
@@ -0,0 +1,23 @@
+import User from "../models/userModel.js";
+
+export const register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email already in use" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
