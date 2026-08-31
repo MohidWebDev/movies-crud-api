0b362ec90534cc148733f8859e89a70d1b388e3a
Mohid Yaseen <mmohid069@gmail.com>
2026-08-31 14:45:17 +0500

    feat(auth): add login controller with JWT issuance

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -16,6 +17,36 @@ export const register = async (req, res, next) => {
       name: user.name,
       email: user.email,
       role: user.role,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = generateToken(user._id, user.role);
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
     });
   } catch (err) {
     next(err);
